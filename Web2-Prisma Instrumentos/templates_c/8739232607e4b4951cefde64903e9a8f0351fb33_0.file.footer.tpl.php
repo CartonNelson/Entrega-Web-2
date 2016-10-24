094cb673aa60582a768734bac914b2b8d18d31ed
floyd144 <nelsoncarton@gmail.com>
2016-10-23 22:45:08 -0300

    cambio templates

---
+++ Web2-Prisma Instrumentos/templates_c/8739232607e4b4951cefde64903e9a8f0351fb33_0.file.footer.tpl.php
@@ -0,0 +1,49 @@
+<?php
+/* Smarty version 3.1.30, created on 2016-10-24 03:34:18
+  from "C:\xampp\htdocs\prisma\Web2-Prisma Instrumentos\templates\footer.tpl" */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.30',
+  'unifunc' => 'content_580d651aae80f0_12195137',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    '8739232607e4b4951cefde64903e9a8f0351fb33' => 
+    array (
+      0 => 'C:\\xampp\\htdocs\\prisma\\Web2-Prisma Instrumentos\\templates\\footer.tpl',
+      1 => 1477107269,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_580d651aae80f0_12195137 (Smarty_Internal_Template $_smarty_tpl) {
+?>
+<footer>
+      <p>Copyright 2016|Todos los derechos reservados Prisma Instrumentos SA.|John Lennon, 7000|Tandil|Buenos Aires|Argentina</p>
+  </footer>
+  <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+  <?php echo '<script'; ?>
+ src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"><?php echo '</script'; ?>
+>
+  <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <?php echo '<script'; ?>
+ src="js/bootstrap.min.js"><?php echo '</script'; ?>
+>
+      <?php echo '<script'; ?>
+ type="text/javascript" src="js/render.js"><?php echo '</script'; ?>
+>
+          <?php echo '<script'; ?>
+ type="text/javascript" src="http://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"><?php echo '</script'; ?>
+>
+          <?php echo '<script'; ?>
+ type="text/javascript" src="js/tabla.js">
+          <?php echo '</script'; ?>
+>
+</body>
+</html>
+<?php }
+}
---
+++ Web2-Prisma Instrumentos/templates_c/ad873c0a54e9aee775683b7702cf3fb0d2805468_0.file.contacto.tpl.php
@@ -0,0 +1,53 @@
+<?php
+/* Smarty version 3.1.30, created on 2016-10-24 03:34:18
+  from "C:\xampp\htdocs\prisma\Web2-Prisma Instrumentos\templates\contacto.tpl" */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.30',
+  'unifunc' => 'content_580d651aac7194_14471714',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    'ad873c0a54e9aee775683b7702cf3fb0d2805468' => 
+    array (
+      0 => 'C:\\xampp\\htdocs\\prisma\\Web2-Prisma Instrumentos\\templates\\contacto.tpl',
+      1 => 1477107269,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_580d651aac7194_14471714 (Smarty_Internal_Template $_smarty_tpl) {
+?>
+<div class="row">
+  <div id="contact">
+    <form id="contacto" class="col-xs-4 col-md-offset-4" method="POST">
+      <h2>Contactanos</h2>
+        <div>
+          <button class="face"></button>
+          <button class="twit" ></button>
+          <button class="inst" ></button>
+        </div>
+        <div class="contacto">
+          <div >
+            <h2>Nombre</h2>
+              <input name="nombre" class="form-control" type="text" value="" size="30" />
+          </div>
+          <div>
+            <h2>E-mail</h2>
+              <input name="email" class="form-control" type="text" value="" size="30" />
+          </div>
+          <div>
+            <h2>Mensaje</h2>
+              <textarea name="mensaje" rows="7" cols="50"></textarea>
+          </div>
+            <input type="submit" class="btn btn-danger"value="Enviar Consulta" />
+        </div>
+    </form>
+  </div>
+</div>
+<?php }
+}
